8df1b1b9a54dc69c7c703c05f5a3409cb0724039
Tushar Rajput <tusharo17rajput@gmail.com>
2022-11-16 23:54:26 +0530

    jwt updated

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,29 @@
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken")
+const User = require("../model/User")
+const CryptoJS = require("crypto-js")
+const { verifyTokenAndAuthorization } = require("./verifyToken")
 
 const router = require("express").Router()
 
 // UPDATE
-router.put("/:id", verifyTokenAndAuthorization, (req, res) => {})
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString()
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+    res.status(200).json(updatedUser)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 
 module.exports = router
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,14 +1,20 @@
 const jwt = require("jsonwebtoken")
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.header.token
+  const authHeader = req.headers.token
   if (authHeader) {
+    const token = authHeader.split(" ")[1]
+    // console.log(token)
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) res.status(403).json("Token is not valid")
+      if (err) {
+        console.log(err)
+        res.status(403).json("Token is not valid")
+      }
       req.user = user
       next()
     })
   } else {
+    // console.log(res.status(401))
     return res.status(401).json("You are not authenticated")
   }
 }
