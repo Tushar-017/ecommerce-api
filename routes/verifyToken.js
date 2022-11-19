baee073265d328e6a57711f9bc0b267deafb0f23
Tushar Rajput <tusharo17rajput@gmail.com>
2022-11-19 20:09:49 +0530

    almost done with the api

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -19,9 +19,23 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next()
     } else {
-      res.status(403).json("You are not alowed to do that!")
+      res.status(403).json("You are not allowed to do that!")
     }
   })
 }
 
-module.exports = { verifyToken, verifyTokenAndAuthorization }
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next()
+    } else {
+      res.status(403).json("You are not allowed to do that!")
+    }
+  })
+}
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+}
