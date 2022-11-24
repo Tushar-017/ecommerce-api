b6dfa03749cd22e1177ec01ce8caefd2c062132b
Tushar Rajput <tusharo17rajput@gmail.com>
2022-11-24 12:59:25 +0530

    stripe error resolved

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,12 +1,15 @@
-const router = required("express").Router()
-const stripe = require("stripe")(process.env.STRIPE_KEY)
+const router = require("express").Router()
+// const stripe = require("stripe")(process.env.STRIPE_KEY)
+const KEY = process.env.STRIPE_KEY
+const stripe = require("stripe")(KEY)
 
-router.post("/payment", (req, res) => {
-  stripe.charges.create(
+router.post("/payment", async (req, res) => {
+  const paymentIntent = await stripe.paymentIntents.create(
     {
-      source: req.body.tokenId,
+      // source: req.body.card.token,
       amount: req.body.amount,
       currency: "usd",
+      payment_method_types: ["card"],
     },
     (stripeErr, stripeRes) => {
       if (stripeErr) {
